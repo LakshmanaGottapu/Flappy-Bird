f4cb01260df6b31041bc30a17ee9e3d312a280ef
LakshmanaGottapu <lakshman.automation@gmail.com>
2024-10-11 13:40:00 +0530

    added top hole to pipe component.

--- pipes.js
+++ pipes.js
@@ -1,11 +1,11 @@
-let PIPE_SPEED = 0.15;
+let PIPE_SPEED = 0.015;
 // const pipes = document.querySelectorAll('.pipe');
 const app = document.getElementById('app');
 const pipes = [];
 export function updatePipe(delta){
     // const pipe = document.createElement('div');
     // pipe.classList.add('pipe');
-    PIPE_SPEED += 0.0001
+    // PIPE_SPEED += 0.0001
     pipes.forEach((pipe, index) => {
         if(getPipePosition(pipe)>window.innerWidth){
             app.removeChild(pipe);
@@ -17,8 +17,20 @@ export function updatePipe(delta){
 export function createPipe(){
     const pipe = document.createElement('div');
     pipe.classList.add('pipe');
+    const topPipe = document.createElement('div');
+    // const bottomPipe = document.createElement('div');
+    topPipe.classList.add('top');
+    // bottomPipe.classList('bottom');
+    topPipe.style.setProperty('--hole-height', randomeHeight());
+    pipe.appendChild(topPipe);
+    // bottomPipe.style.setProperty('--hole-height', randomeHeight());
     app.appendChild(pipe);
     pipes.push(pipe);
+}
+function randomeHeight(){
+    const min = 0.3;
+    const max = 0.6;
+    return (min+(max-min)*Math.random())*window.innerHeight;
 }
 function setPipePosition(pipe, position){
     pipe.style.setProperty('--pipe-right', position) ;
